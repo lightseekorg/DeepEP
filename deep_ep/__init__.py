c14d809197259863c2e6cd16c78fff9c988fd5f0
sjqgogogogo <sjqgogogogo@gmail.com>
2026-09-10 07:58:50 +0800

    Support Kimi K3 legacy routing and optional legacy-only builds (#1)
    Signed-off-by: sjqgogogogo <257329582+sjqgogogogo@users.noreply.github.com>

--- deep_ep/__init__.py
+++ deep_ep/__init__.py
@@ -86,12 +86,18 @@ init_jit()
 
 # Import APIs after initialization
 from .buffers.legacy import Buffer
-from .buffers.elastic import ElasticBuffer, EPHandle
+_legacy_only = bool(int(os.environ.get('EP_LEGACY_ONLY', '0')))
+if _legacy_only:
+    ElasticBuffer = None
+    EPHandle = None
+else:
+    from .buffers.elastic import ElasticBuffer, EPHandle
 # noinspection PyUnresolvedReferences
 from .utils.event import EventOverlap, EventHandle
-from .utils.envs import get_physical_domain_size, get_logical_domain_size
+if not _legacy_only:
+    from .utils.envs import get_physical_domain_size, get_logical_domain_size
 
 # noinspection PyUnresolvedReferences
 from deep_ep._C import Config, topk_idx_t
 
-__version__ = '2.1.0'
+__version__ = '2.1.1'
